9dcdf9b8143a59ddd48c71b30f9d0baf6827381f
Bilal Sanayu Majid <bilalsanayumajid@gmail.com>
2026-04-25 15:28:26 +0700

    Refactor: Membuat PageHeader menjadi komponen reusable

--- src/screens/ProfilePage.jsx
+++ src/screens/ProfilePage.jsx
@@ -51,7 +51,7 @@ export default function ProfilePage({ user, onLogout }) {
                                     <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
                                     <circle cx="12" cy="7" r="4" />
                                 </svg>
-                                <h3>Informasi Pribadi</h3>
+                                <h3>Informasi Pribadi</h3> 
                             </div>
 
                             <div className="profile-info-grid">
